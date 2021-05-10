9522e2e25c6e16e4ee6ef6e936dc5d8586078b0c
kayarez <tansteppp@gmail.com>
2021-05-10 13:43:45 +0300

    Add main page on React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Header } from './components/Header';
+import { Balances } from './components/Balances';
+import { IncomeConsumption } from './components/IncomeConsumption';
+import { Transaction } from './components/Transaction';
+import { AddTransaction } from './components/AddTransaction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+        <Header />
+              <div className="container">
+        <Balances />
+        <IncomeConsumption />
+        <Transaction />
+        <AddTransaction />
+        </div>
+        </div>
+
   );
 }
 
